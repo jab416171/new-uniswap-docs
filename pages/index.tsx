9c24978636f373c47afe275780c4d59c41575060
jab416171 <jab416171@gmail.com>
2021-06-08 13:54:37 -0600

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
 import { GetStaticProps } from 'next'
+import { usePlugin } from 'tinacms'
+import { useGithubJsonForm, useGithubToolbarPlugins } from 'react-tinacms-github'
 
 export default function Home({ file }) {
-  const data = file.data
+    const formOptions = {
+      label: 'Home Page',
+      fields: [{ name: 'title', component: 'text' }],
+    }
+    const [data, form] = useGithubJsonForm(file, formOptions)
+    usePlugin(form)
+    useGithubToolbarPlugins()
   return (
     <div className={styles.container}>
       <Head>
